649b1fa0b38965d38f806b1173af24f30a0e9d50
kyoohyun <kgh2120@naver.com>
2023-01-17 15:18:19 +0900

    init

---
+++ src/main/java/com/gaaji/useditem/UseditemApplication.java
@@ -0,0 +1,13 @@
+package com.gaaji.useditem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UseditemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UseditemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gaaji/useditem/UseditemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaaji.useditem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UseditemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
